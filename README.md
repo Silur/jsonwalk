876581abdceb2a3050b997f406a26216942fa8dd
Silur <abrahamendre9@gmail.com>
2020-09-05 02:16:25 +0200

    add README

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # jsonwalk
-Recursively traverse JSON data with jq syntax and apply functions on it's fields
+
+This tool allows you to walk a JSON object recursively given a jq-like query array, and run function transforms on the fields contained in this schema.
+
+## Example
+
+```javascript
+
+const jsonwalk = require('jsonwalk')
+
+const myJson = {
+	"Product Name": "Bowler Hat",
+    "ProductID": 858383,
+    "SKU": "0406654608",
+    "Description": {
+    	"Colour": "Purple",
+    	"Width": 300,
+    	"Height": 200,
+    	"Depth": 210,
+    	"Weight": 0.75
+     },
+    "Price": 34.45,
+    "Quantity": 2,
+	"tags": ['clothing', 'hats']
+
+}
+
+const query = ['Description.Width', 'Description.Height']
+
+function makeBigger(obj, index, elem) {
+	console.log(elem)
+	return elem*2 // double the size
+}
+
+jsonwalk.walk(myJson, query, makeBigger)
+```
+
+Like in `jq`, you can specify array elements with `$`:
+
+```javascript
+
+const query = ['Description.tags.$]
+
+function makeBigger(obj, index, elem) {
+	console.log(elem)
+	return elem.toUpperCase()
+}
+
+jsonwalk.walk(myJson, query, makeBigger)
+```
--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-module.exports = {
-  walk: async function (obj, schema, op, path) {
-    if (typeof path === 'undefined') path = []
-    for (const i in obj) {
-      if (typeof obj[i] === 'object') {
-        path.push(i)
-        this.walk(obj[i], schema, op, path)
-        path.splice(-1, 1)
-      } else {
-        for (const pi in schema) {
-          const absolutePath = path.concat([i]).join('.')
-          const normalizedPath = absolutePath
-            .replace(/\.\d+\./g, '.$.') // .0, .1 etc are not valid JSON keys
-          if (schema[pi] === normalizedPath) {
-            obj[i] = await op(obj, i, obj[i])
-          }
+async function walk (obj, schema, op, path) {
+  if (typeof path === 'undefined') path = []
+  for (const i in obj) {
+    if (typeof obj[i] === 'object') {
+      path.push(i)
+      walk(obj[i], schema, op, path)
+      path.splice(-1, 1)
+    } else {
+      const absolutePath = path.concat([i]).join('.')
+      // console.log('abs:', path.concat([i]))
+      const normalizedPath = absolutePath
+        .replace(/\.\d+\./g, '.$.') // .0, .1 etc are not valid JSON keys
+      for (const pi in schema) {
+        if (schema[pi] === normalizedPath) {
+          obj[i] = await op(obj, i, obj[i])
         }
       }
     }
-    return obj
   }
+  return obj
+}
+
+module.exports = {
+  walk: walk
 }
